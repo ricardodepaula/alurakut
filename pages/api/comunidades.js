6c2d1f43fbb0161bcedc3bd28d56714847f41237
Ricardo de Almeida Paula <ricardo.paula@mpce.mp.br>
2021-07-15 22:58:55 -0300

    Aula 04

---
+++ pages/api/comunidades.js
@@ -0,0 +1,31 @@
+import { SiteClient } from 'datocms-client';
+
+export default async function recebedorDeRequests(request, response) {
+
+    if(request.method === 'POST') {
+
+        const TOKEN = '400902104ee1674cef67ebe3dff877';
+        const client = new SiteClient(TOKEN);
+
+        console.log(TOKEN);
+        console.log('Após o concole log do TOKEN');
+
+        // Validar os dados, antes de sair cadastrando
+        const registroCriado = await client.items.create({
+            itemType: "967698", // ID do Model de "Communities" criado pelo Dato
+            ...request.body,
+            title: "Comunidade de Teste II via API",
+            imageUrl: "https://github.com/ricardodepaula.png",
+            creatorslug: "ricardodepaula"
+        })
+
+        console.log(registroCriado);
+
+        return;
+    }
+
+    response.status(404).json({
+        message: 'Ainda não temos nada no GET, mas no POST tem!'
+    })
+
+} 
